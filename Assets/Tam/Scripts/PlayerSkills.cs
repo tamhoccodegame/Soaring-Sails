3bff70d0e1ac03381eebafd54419981140411b23
Tam Doan <tamdoan246@gmail.com>
2024-04-16 19:32:12 +0700

    SkillTree done, doing Level System

---
+++ Assets/Tam/Scripts/PlayerSkills.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class PlayerSkills
+{
+	public event EventHandler OnSkillPointChange;
+	public event EventHandler<OnSkillUnlockedEvenetArgs> OnSkillUnlocked;
+	public class OnSkillUnlockedEvenetArgs : EventArgs
+	{
+		public SkillType skillType;
+	}
+
+   public enum SkillType
+	{
+		None,
+		skill1,
+		skill2,
+		skill3,
+		HealthMax_1,
+		HealthMax_2,
+		HealthMax_3,
+		MovementSpeed_1,
+		MovementSpeed_2,
+		MovementSpeed_3,
+		Damage_1,
+		Damage_2,
+		Damage_3,
+	}
+
+	private List<SkillType> unlockedSkillTypeList;
+	private int skillPoints = 1000;
+
+	public PlayerSkills()
+	{
+		unlockedSkillTypeList = new List<SkillType>();
+	}
+
+	public void AddSkillPoint()
+	{
+		skillPoints++;
+		OnSkillPointChange?.Invoke(this, EventArgs.Empty);
+	}
+
+	public int GetSkillPoints()
+	{
+		return skillPoints;
+	}
+
+	private void UnlockSkill(SkillType skillType)
+	{
+		if (!IsSkillUnlocked(skillType))
+		{
+			unlockedSkillTypeList.Add(skillType);
+			OnSkillUnlocked?.Invoke(this, new OnSkillUnlockedEvenetArgs { skillType = skillType });
+		}
+		
+	}
+
+	public bool IsSkillUnlocked(SkillType skillType)
+	{
+		return unlockedSkillTypeList.Contains(skillType);
+	}
+
+	public bool CanUnlock(SkillType skillType)
+	{
+		SkillType skillRequirement = GetSkillRequirement(skillType);
+
+		if (!IsSkillUnlocked(skillType))
+		{
+			if (skillRequirement != SkillType.None)
+			{
+				if (IsSkillUnlocked(skillRequirement))
+				{
+					return true;
+				}
+				else
+				{
+					Debug.Log("Upgrade " +  skillRequirement + " first");
+					return false;
+				}
+			}
+			else
+			{
+				return true;
+			}
+		}
+		else
+		{
+			return false;
+		}
+		
+	}
+
+	public SkillType GetSkillRequirement(SkillType skillType)
+	{
+		switch (skillType)
+		{
+			case SkillType.HealthMax_3:			return SkillType.HealthMax_2;
+			case SkillType.HealthMax_2:			return SkillType.HealthMax_1;
+			case SkillType.Damage_3:			return SkillType.Damage_2;
+			case SkillType.Damage_2:			return SkillType.Damage_1;
+			case SkillType.MovementSpeed_3 :	return SkillType.MovementSpeed_2;
+			case SkillType.MovementSpeed_2:		return SkillType.MovementSpeed_1;
+		}
+		return SkillType.None;
+	}
+
+	public bool TryUnlockSkill(SkillType skillType)
+	{
+		SkillType skillRequirement = GetSkillRequirement(skillType);
+		if (CanUnlock(skillType))
+		{
+			if(skillPoints > 0)
+			{
+				skillPoints--;
+				UnlockSkill(skillType);
+				OnSkillPointChange?.Invoke(this, EventArgs.Empty);
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+			
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
---
+++ Assets/Tam/Scripts/UI_SkillTree.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CodeMonkey.Utils;
+using TMPro;
+public class UI_SkillTree : MonoBehaviour
+{
+	[SerializeField] private Material skillLockedMaterial;
+	[SerializeField] private Material skillUnlockableMaterial;
+	[SerializeField] private SkillUnlockPath[] skillUnlockPathArray;
+	[SerializeField] private Sprite lineSprite;
+	[SerializeField] private Sprite lineGlowSprite;
+
+	private PlayerSkills playerSkills;
+	private List<SkillButton> skillButtonList;
+	private TextMeshProUGUI skillPointsText;
+	private void Awake()
+	{
+		
+	}
+
+	private void PlayerSkills_OnSkillPointChange(object sender, System.EventArgs e)
+	{
+		UpdateSkillPoints();
+	}
+
+	public void SetPlayerSkills(PlayerSkills playerSkills)
+	{
+		this.playerSkills = playerSkills;
+
+		skillButtonList = new List<SkillButton>();
+		skillButtonList.Add(new SkillButton(transform.Find("HealthMax_1"), playerSkills, PlayerSkills.SkillType.HealthMax_1, skillUnlockableMaterial, skillLockedMaterial));
+		skillButtonList.Add(new SkillButton(transform.Find("HealthMax_2"), playerSkills, PlayerSkills.SkillType.HealthMax_2, skillUnlockableMaterial, skillLockedMaterial));
+		skillButtonList.Add(new SkillButton(transform.Find("HealthMax_3"), playerSkills, PlayerSkills.SkillType.HealthMax_3, skillUnlockableMaterial, skillLockedMaterial));
+		skillButtonList.Add(new SkillButton(transform.Find("MovementSpeed_1"), playerSkills, PlayerSkills.SkillType.MovementSpeed_1, skillUnlockableMaterial, skillLockedMaterial));
+		skillButtonList.Add(new SkillButton(transform.Find("MovementSpeed_2"), playerSkills, PlayerSkills.SkillType.MovementSpeed_2, skillUnlockableMaterial, skillLockedMaterial));
+		skillButtonList.Add(new SkillButton(transform.Find("MovementSpeed_3"), playerSkills, PlayerSkills.SkillType.MovementSpeed_3, skillUnlockableMaterial, skillLockedMaterial));
+		skillButtonList.Add(new SkillButton(transform.Find("Damage_1"), playerSkills, PlayerSkills.SkillType.Damage_1, skillUnlockableMaterial, skillLockedMaterial));
+		skillButtonList.Add(new SkillButton(transform.Find("Damage_2"), playerSkills, PlayerSkills.SkillType.Damage_2, skillUnlockableMaterial, skillLockedMaterial));
+		skillButtonList.Add(new SkillButton(transform.Find("Damage_3"), playerSkills, PlayerSkills.SkillType.Damage_3, skillUnlockableMaterial, skillLockedMaterial));
+
+		playerSkills.OnSkillUnlocked += PlayerSkills_OnSkillUnlocked;
+		playerSkills.OnSkillPointChange += PlayerSkills_OnSkillPointChange;
+		UpdateVisuals();
+	}
+
+	private void PlayerSkills_OnSkillUnlocked(object sender, PlayerSkills.OnSkillUnlockedEvenetArgs e)
+	{
+		UpdateVisuals();
+	}
+
+	private void UpdateVisuals()
+	{
+		foreach(SkillButton skillButton in skillButtonList)
+		{
+			skillButton.UpdateVisuals();
+		}
+
+		foreach(SkillUnlockPath skillUnlockPath in skillUnlockPathArray)
+		{
+			foreach(Image linkImage in skillUnlockPath.linkImageArray)
+			{
+				linkImage.color = new Color(.5f, .5f, .5f);
+				linkImage.sprite = lineSprite;
+			}
+		}
+
+		foreach (SkillUnlockPath skillUnlockPath in skillUnlockPathArray)
+		{
+			if(playerSkills.IsSkillUnlocked(skillUnlockPath.skillType) || playerSkills.CanUnlock(skillUnlockPath.skillType))
+			{
+				foreach(Image linkImage in skillUnlockPath.linkImageArray)
+				{
+					linkImage.color = Color.white;
+					linkImage.sprite = lineGlowSprite;
+				}
+			}
+		}
+	}
+
+	public void UpdateSkillPoints()
+	{
+		skillPointsText.SetText(playerSkills.GetSkillPoints().ToString());	
+	}
+
+	
+
+	private class SkillButton
+	{
+		private Transform transform;
+		private Image image;
+		private PlayerSkills playerSkills;
+		private PlayerSkills.SkillType skillType;
+		private Material skillUnlockableMaterial;
+		private Material skillLockedMaterial;
+
+		public SkillButton(Transform transform, PlayerSkills playerSkills, PlayerSkills.SkillType skillType, Material skillUnlockableMaterial, Material skillLockedMaterial)
+		{
+			this.transform = transform;
+			this.playerSkills = playerSkills;
+			this.skillType = skillType;
+
+			transform.GetComponent<Button_UI>().ClickFunc = () =>
+			{
+				playerSkills.TryUnlockSkill(skillType);
+			};
+		}
+
+		public void UpdateVisuals()
+		{
+			//if(playerSkills.IsSkillUnlocked(skillType))
+			//{
+			//	image.material = null;
+			//}
+			//else
+			//{
+			//	if (playerSkills.CanUnlock(skillType)){
+			//		image.material = skillUnlockableMaterial;
+			//	}
+			//	else
+			//	{
+			//		image.material = skillLockedMaterial;
+			//	}
+			//}
+		}
+	}
+
+	[System.Serializable]
+	public class SkillUnlockPath
+	{
+		public PlayerSkills.SkillType skillType;
+		public Image[] linkImageArray;
+	}
+}
