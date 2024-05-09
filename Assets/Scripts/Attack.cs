8569eafaceaa117abf93967bade1128e9b7a756e
Tam Doan <118838652+tamhoccodegame@users.noreply.github.com>
2024-05-09 16:27:31 +0700

    MergeLong

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -4,38 +4,67 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    public Weapon1 weapon1;
-    bool onceAtk = true;
-    float atkSpeed = 1;
-    private AudioManager audioManager;
+	public Weapon1 weapon1;
+	bool onceAtk = false;
+	[SerializeField] float atkSpeed;
+	public float atkSpeed2;
 
 
 
-	private void Start()
-	{
-		audioManager = GetComponent<AudioManager>();
-	}
+
 
 	void Update()
-    {
-        atkSpeed = GetComponentInParent<PlayerController1>().atkSpeed;
-        if (onceAtk == true)
-        {
-           
-            if (Input.GetMouseButtonDown(0))
-            {
-                audioManager.PlayAudioClip("attack");
-                StartCoroutine(DelayAtk(atkSpeed));
-				onceAtk = false;
-            }       
-           
-        }
-    }
-    private IEnumerator DelayAtk(float atkSpeed)
-    {
-		weapon1.Attack();   
-        yield return new WaitForSeconds(atkSpeed); // Độ trễ 1 giây    
-        onceAtk = true;
-        
-    }  
+	{
+		atkSpeed = GetComponentInParent<PlayerController1>().atkSpeed;
+		if (onceAtk == true)
+
+			if (Input.GetMouseButtonDown(0) && atkSpeed2 < atkSpeed)
+			{
+
+				if (onceAtk == true)
+				{
+					atkSpeed2 = atkSpeed;
+				}
+				if (onceAtk == true)
+				{
+					weapon1.Attack();
+					onceAtk = false;
+				}
+			}
+		if (atkSpeed2 <= 0 && onceAtk == false)
+		{
+			onceAtk = true;
+		}
+		else
+		{
+			atkSpeed2 -= Time.deltaTime;
+		}
+
+	}
+
 }
+/* if (onceAtk == true)
+	{         
+		if (Input.GetMouseButtonDown(0))
+		{         
+			StartCoroutine(DelayAtk(atkSpeed));
+			onceAtk = false;
+onceAtk = false;
+		}       
+
+	}
+}
+private IEnumerator DelayAtk(float atkSpeed)
+{    
+
+		weapon1.Attack();   
+	yield return new WaitForSeconds(atkSpeed); // Độ trễ 1 giây    
+	onceAtk = true;
+}  
+}
+{
+
+weapon1.Attack();
+yield return new WaitForSeconds(atkSpeed); // Độ trễ 1 giây    
+onceAtk = true;
+}  */
